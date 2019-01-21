fb5012ca843f7b9abc472dc7da281a7f739c5eff
Felipe Santiago <jsfelipe@gmail.com>
2019-01-20 22:22:12 -0300

    atualizacao

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -11,6 +11,42 @@ class AuthController {
 
         return response.status(201).send({ data: user })
     }
+
+    async login({ request, response, auth }) {
+        const { email, password } = request.all()
+
+        const user = await auth.withRefreshToken().attempt(email, password)
+
+        return response.send({ data: user })
+    }
+
+    async refresh({ request, response, auth }) {
+        const refresh_token = request.input('refresh_token')
+
+        if (!refresh_token) {
+            refresh_token = request.header('refresh_token')
+        }
+
+        const user = await auth
+            .newRefreshToken()
+            .generateForRefreshToken(refresh_token)
+
+        return response.send({ data: user })
+    }
+
+    async logout({ request, response, auth }) {
+        const refresh_token = request.input('refresh_token')
+
+        if (!refresh_token) {
+            refresh_token = request.header('refresh_token')
+        }
+
+        const loggedOut = await auth
+            .authenticator('jwt')
+            .revokeTokens([refresh_token], true)
+
+        return response.send({ message: 'User Logged Out!' })
+    }
 }
 
 module.exports = AuthController
